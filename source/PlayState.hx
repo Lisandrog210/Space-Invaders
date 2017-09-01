320a7205fe4f517056473848226b094a008efec2
Unknown <agustingarcia1997@gmail.com>
2017-09-01 17:12:02 -0300

    Enemies implemented

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,26 @@
 package;
 
+import entities.Enemies;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 class PlayState extends FlxState
 {
+	var enemigo:Enemies;
+	var GrupoEne:FlxTypedGroup<Enemies>;
+	
 	override public function create():Void
 	{
 		super.create();
+		
+		GrupoEne = new FlxTypedGroup<Enemies>();
+		
+		for (i in 0...5) 
+		{
+			var enem: Enemies = new Enemies(i*20, 0, AssetPaths.nave1__png);
+			add(enem);
+		}
+		
 	}
 
 	override public function update(elapsed:Float):Void
--- source/entities/Enemies.hx
+++ source/entities/Enemies.hx
@@ -4,11 +4,39 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 
-class Enemies 
-{
+/**
+ * ...
+ * @author ...
+ */
 
-	public function new() 
+
+
+class Enemies extends FlxSprite 
+{
+	private var vel:Float = 0.25;
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
+		super(X, Y, SimpleGraphic);
+		
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+		
+		move();
+	}
+	
+	public function move()
+	{
+		while (y < FlxG.height) 
+		{
+			y = y + vel;
+			break;
+		}
+		
+		
 		
 	}
 	
