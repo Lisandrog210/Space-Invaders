ced355537f460717f445f906610705b50dbbc846
Unknown <li_gueva@hotmail.com>
2017-09-08 15:34:44 -0300

    splash de flixel del comienzo removido

--- source/Main.hx
+++ source/Main.hx
@@ -9,6 +9,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(160, 144, states.PlayState, 4));
+		addChild(new FlxGame(160, 144, states.PlayState, 4, 60, 60, true));
 	}
 }
